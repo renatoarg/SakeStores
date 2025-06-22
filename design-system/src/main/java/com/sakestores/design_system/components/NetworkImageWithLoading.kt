d7112a1ef9089d6b3729fea461702604d4625ff4
Renato Goncalves <renatoarg@gmail.com>
2025-06-22 10:07:54 -0300

    loading images

---
+++ design-system/src/main/java/com/sakestores/design_system/components/NetworkImageWithLoading.kt
@@ -0,0 +1,61 @@
+package com.sakestores.design_system.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.sakestores.design_system.R
+
+@Composable
+fun NetworkImageWithLoading(
+    imageUrl: String?,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Crop,
+    placeholderRes: Int = R.drawable.placeholder2
+) {
+    var isLoading by remember { mutableStateOf(true) }
+
+    Box(modifier = modifier) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .error(placeholderRes)
+                .fallback(placeholderRes)
+                .build(),
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            modifier = Modifier.matchParentSize(),
+            onState = { state ->
+                isLoading = state is AsyncImagePainter.State.Loading
+            }
+        )
+
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(32.dp),
+                strokeWidth = 2.dp
+            )
+        }
+    }
+}
+
--- feat-sake-list/src/main/java/com/sakestores/feat_sake_list/ui/SakeShopCard.kt
+++ feat-sake-list/src/main/java/com/sakestores/feat_sake_list/ui/SakeShopCard.kt
@@ -8,35 +8,34 @@ import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import com.sakestores.design_system.R.*
+import com.sakestores.design_system.components.NetworkImageWithLoading
 import com.sakestores.design_system.theme.SakeStoresTheme
 import com.sakestores.domain.model.SakeShop
 
@@ -53,19 +52,40 @@ fun SharedTransitionScope.SakeShopCard(
             .padding(bottom = 16.dp)
             .clickable { onClick() },
     ) {
-        AsyncImage(
-            model = sakeShop.picture,
+//        AsyncImage(
+//            model = ImageRequest.Builder(LocalContext.current)
+//                .data(sakeShop.picture)
+//                .crossfade(true)
+//                .placeholder(drawable.placeholder2)
+//                .memoryCachePolicy(CachePolicy.ENABLED)
+//                .diskCachePolicy(CachePolicy.ENABLED)
+//                .fallback(drawable.placeholder2)
+//                .error(drawable.placeholder2)
+//                .build(),
+//            contentDescription = sakeShop.name,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(160.dp)
+//                .padding(horizontal = 16.dp)
+//                .sharedElement(
+//                    state = rememberSharedContentState("image/${sakeShop.name}"),
+//                    animatedVisibilityScope = animatedVisibilityScope
+//                )
+//                .background(MaterialTheme.colorScheme.surfaceVariant),
+//            contentScale = ContentScale.Crop
+//        )
+        NetworkImageWithLoading(
+            imageUrl = sakeShop.picture,
             contentDescription = sakeShop.name,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(160.dp)
                 .padding(horizontal = 16.dp)
-                .sharedElement(
+                .background(MaterialTheme.colorScheme.surfaceVariant)
+                            .sharedElement(
                     state = rememberSharedContentState("image/${sakeShop.name}"),
                     animatedVisibilityScope = animatedVisibilityScope
                 )
-                .background(MaterialTheme.colorScheme.surfaceVariant),
-            contentScale = ContentScale.Crop
         )
         Text(
             text = sakeShop.name,
